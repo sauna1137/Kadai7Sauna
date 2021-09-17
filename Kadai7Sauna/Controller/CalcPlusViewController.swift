fc48a781139cecc2ae1b87159094db64fdc162d1
sauna <soneko2102@gmail.com>
2021-09-17 18:22:32 +0900

    フィードバックを受け、enumを追加し新たにtextField2を作成

--- Kadai7Sauna/Controller/CalcPlusViewController.swift
+++ Kadai7Sauna/Controller/CalcPlusViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 final class CalcPlusViewController: UIViewController {
-    @IBOutlet private var textFIelds: [UITextField]!
+
+    @IBOutlet weak private var textField1: UITextField!
+    @IBOutlet weak private var textField2: UITextField!
     @IBOutlet private weak var resultLabel: UILabel!
 
     private let calcModel = CalcModel()
 
     @IBAction private func showResultButtonDidTapped(_ sender: Any) {
-        resultLabel.text = calcModel.calculate(num1: Int(textFIelds[0].text ?? "") ?? 0, num2: Int(textFIelds[1].text ?? "") ?? 0, sign: "plus")
+        resultLabel.text = calcModel.calculate(num1: Int(textField1.text ?? "") ?? 0, num2: Int(textField2.text ?? "") ?? 0, sign: .plus)
         view.endEditing(true)
     }
 
